072b8a4c4cbb4b1ba48895d9cb2951f0b7366ae7
Edward Tanguay <edward@tanguay.info>
2022-06-17 16:25:07 +0200

    Edward: removed edward2 page

--- src/pages/members/PageMembers.jsx
+++ src/pages/members/PageMembers.jsx
@@ -8,8 +8,6 @@ export const PageMembers = () => {
 				<NavLink to="info">Info</NavLink>
 				<Separator />
 				<NavLink to="edward">Edward</NavLink>
-				<Separator />
-				<NavLink to="edward2">Edward2</NavLink>
 			</nav>
 			<Outlet />
 		</div>
