5e62a540e6de04886eca409ae01c3fc7bffbc073
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-17 11:25:54 +0200

    finished

--- src/pages/PageResearch.jsx
+++ src/pages/PageResearch.jsx
@@ -4,6 +4,8 @@ export const PageResearch = () => {
 	return (
 		<div className="page_research">
 			<p>These are new tools, libraries, etc. that members are experimenting with. Projects here are generally not complete. For that, see the <NavLink to="/showcases">Showcases</NavLink> page.</p>
+
+			<p>Hey, do you want to try out socket.io and cheerio library.</p>
 		</div>
 	)
 }
